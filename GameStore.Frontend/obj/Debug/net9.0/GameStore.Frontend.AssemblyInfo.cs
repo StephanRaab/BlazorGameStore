fc7b6db88282bde4fb1b242e796ab8eec41e78a6
Stephan Raab <sarcartist@gmail.com>
2024-12-09 20:44:21 -0500

    wow...I'm going to need to update this gitignore file...

--- GameStore.Frontend/obj/Debug/net9.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net9.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915ca13d16b96ee49d581f07c201d2489adfeba0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d540d9d04d141fcebccbadbfa93b21ad976aecc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
